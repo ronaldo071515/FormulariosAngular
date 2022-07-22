eafd9eec923c8dd16d9cbcb2f07fb1b1b212f1eb
Ronaldo Torres <rtorres66@misena.edu.co>
2022-07-22 09:38:35 -0500

    Implementando lazy load y inicio de formularios por tamplate

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { SidemenuComponent } from './sidemenu/sidemenu.component';
+
+
+
+@NgModule({
+  declarations: [
+    SidemenuComponent
+  ],
+  exports: [
+    SidemenuComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ]
+})
+export class SharedModule { }
