f744f1236baaf4aa0f58e1656cf9a785a9161112
Ronaldo Torres <rtorres66@misena.edu.co>
2022-07-22 18:34:03 -0500

    Fin: Formularios por template

--- src/app/template/basicos/basicos.component.ts
+++ src/app/template/basicos/basicos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -9,13 +9,36 @@ import { NgForm } from '@angular/forms';
 })
 export class BasicosComponent implements OnInit {
 
+  @ViewChild('miFormulario') miFormulario!: NgForm;
+  initForm = {
+    producto: 'RTX 4080ti',
+    precio: 0,
+    existencias: 10
+  }
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  guardar( miFormulario: NgForm ) {
-    console.log(miFormulario.value);
+  nombreValido(): boolean {
+    return this.miFormulario?.controls['producto']?.invalid && 
+           this.miFormulario?.controls['producto']?.touched
+  }
+
+  precioValido(): boolean {
+    return this.miFormulario?.controls['precio']?.touched &&
+           this.miFormulario?.controls['precio']?.value < 0
+  }
+
+  /* guardar( miFormulario: NgForm ) { */
+  guardar() {
+    //console.log(this.miFormulario.value);
+    console.log('Posteo correcto');
+    this.miFormulario.resetForm({
+      precio: 0,
+      existencias: 0
+    });
   }
 
 }
