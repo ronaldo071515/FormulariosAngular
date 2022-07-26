6a786bb75032907450b741b639993414c7fd817f
Ronaldo Torres <rtorres66@misena.edu.co>
2022-07-25 20:26:19 -0500

    Fin formularios por template -Dinamicos

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,4 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+
+interface Persona {
+  nombre: string;
+  favoritos: Favorito[];
+}
+
+interface Favorito {
+  id: number;
+  nombre: string;
+}
 
 @Component({
   selector: 'app-dinamicos',
@@ -6,11 +18,41 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
 
-  constructor() { }
+  @ViewChild('miFormulario') miFormulario!: NgForm;
 
-  ngOnInit(): void {
+  nuevoJuego: string = '';
+
+  persona: Persona = {
+    nombre: 'Ronaldo',
+    favoritos: [
+      {
+        id: 1,
+        nombre: 'Halo'
+      },
+      {
+        id: 2,
+        nombre: 'Free Fire'
+      },
+    ]
+  }
+
+  guardar() {
+    console.log('Post del formulario')
+  }
+
+  eliminar( index: number ) {
+    this.persona.favoritos.splice(index, 1);
+  }
+
+  agregarJuego() {
+    const juegoFav: Favorito = {
+      id: this.persona.favoritos.length + 1,
+      nombre: this.nuevoJuego
+    };
+    this.persona.favoritos.push( { ...juegoFav } );
+    this.nuevoJuego = '';
   }
 
 }
